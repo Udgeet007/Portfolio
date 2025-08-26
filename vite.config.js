03579bb37b1c288512ddece6727c45efa1670373
Udgeet <bhatt8udgeet@gmail.com>
2025-08-26 17:24:16 +0530

    frontend setup tailwind setup

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+ import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+   resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'), // Alias '@' to the 'src' directory
+          // Add other aliases as needed, e.g., '@components': path.resolve(__dirname, './src/components')
+        },
+      },
+})
