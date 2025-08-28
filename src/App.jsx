ee50227c4d5a248baf496e6bf423e41b6d0c0742
Udgeet <bhatt8udgeet@gmail.com>
2025-08-28 12:05:40 +0530

    ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NotFound from "./pages/NotFound"
+import Home from "./pages/Home"
 
 
 function App() {
 
 
   return (
-    <div>
-      
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index  element={<Home/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }    
 
